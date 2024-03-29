c5e6956b1c4c089c2c27f753b09234d0867cb408
Esat14 <esat.albayrak14@gmail.com>
2024-03-29 15:35:06 +0300

    after api

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let istenilenSure = 43200;
 let toplamSaniye = istenilenSure * 60;
 let sayacElement = document.querySelector("#sayac-container");
@@ -23,7 +22,6 @@ let sayacBaslat = setInterval(() => {
     productDiscountHrs.innerHTML = `${format(saat)}`;
     productDiscountMin.innerHTML = `${format(dakika)}`;
     productDiscountSec.innerHTML = `${format(saniye)}`;
- 
   }
 }, 1000);
 
@@ -31,3 +29,48 @@ function format(pm) {
   return pm < 10 ? `0${pm}` : pm;
 }
 
+let denemeDivi = document.querySelector("#jsdeneme");
+
+let allProducts = [];
+
+async function getProducts() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
+  allProducts = data;
+  renderTodays();
+}
+
+getProducts();
+
+function renderTodays() {
+  const flashSaleSlideProduct = allProducts.slice(0,12);
+  const yazDostumBunlara = flashSaleSlideProduct
+    .map((products) => {
+      return `
+      <div class="card-for-products">
+        <div class="products-img-container">
+            <p class="card-discount-rate">-40%</p>
+          <img src= ${products.image} alt= ${products.title}
+          class="red-gamepad-img">
+          <div class="products-ispect-box">
+            <img src="./images/wishlist-icon.svg" alt="">
+            <img src="./images/icon-eye.svg" alt="">
+          </div>
+        </div>
+        <h4> ${products.title}</h4>
+        <p class="gamepad-new-price">${products.price}<span class="gamepad-old-price"><s>${products.price}</s></span></p>
+        <div class="star-for-product">
+                <img src="./images/star-for-vote.png" alt="">
+                <img src="./images/star-for-vote.png" alt="">
+                <img src="./images/star-for-vote.png" alt="">
+                <img src="./images/star-for-vote.png" alt="">
+                <img src="./images/star-for-vote.png" alt="">
+                <span>(88)</span>
+        </div>
+      </div>
+    `;
+    })
+    .join("");
+
+  denemeDivi.innerHTML = yazDostumBunlara;
+}
