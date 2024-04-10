0b5fd4b3d2a0c7409643d7ada12daab0bf1712f3
Esat14 <esat.albayrak14@gmail.com>
2024-04-10 15:53:16 +0300

    add font-family and hover effect

--- app.js
+++ app.js
@@ -1,93 +1,40 @@
-// const kutuKutu = document.querySelectorAll(".category-list");
 
-// kutuKutu.addEventListener("mouseenter", (event) => {
-//   const path = event.target.querySelector(".category-svg-path");
 
-//   if (path) {
-//     path.style.stroke = "white";
-//   }
-// });
+const categoriesBox = document.querySelectorAll(".category-list");
 
-// console.log(kutuKutu);
+categoriesBox.forEach((categories) => {
+  categories.addEventListener("mouseenter", (event) => {
+    const categoriesPaths = categories.querySelectorAll(".category-svg-path");
+    const categoriesTitle = categories.querySelectorAll(".slider-category-name");
 
-// const kutuKutu = document.querySelectorAll(".category-list");
-
-// kutuKutu.forEach(kutu => {
-//   kutu.addEventListener("mouseenter", (event) => {
-//     const path = event.target.querySelector(".category-svg-path");
-
-//     if (path) {
-//       path.style.stroke = "white";
-//       path.style.line.stroke = "white";
-//     }
-//   });
-// });
-
-// const kutuKutu = document.querySelectorAll(".category-list");
-
-// kutuKutu.forEach((kutu) => {
-//   kutu.addEventListener("mouseenter", (event) => {
-//     const paths = event.currentTarget.querySelectorAll(".category-svg-path");
-//     const metoText = event.currentTarget.querySelectorAll(".slider-category-name");
-//     if (paths) {
-//       paths.forEach((path) => {
-//         path.style.stroke = "white";
-//       });
-//     }
-//     if (metoText) {
-//       metoText.forEach((metoText) => {
-//         metoText.style = "white";
-//       });
-//     }
-
-//     kutu.style.backgroundColor = "red";
-//   });
-//   kutu.addEventListener("mouseleave", (event) => {
-//     const paths = event.currentTarget.querySelectorAll(".category-svg-path");
-//     if (paths) {
-//       paths.forEach((path) => {
-//         path.style.stroke = "black";
-//       });
-//     }
-//     event.currentTarget.style.backgroundColor = "white";
-//   });
-// });
-
-const kutuKutu = document.querySelectorAll(".category-list");
-
-kutuKutu.forEach((kutu) => {
-  kutu.addEventListener("mouseenter", (event) => {
-    const paths = kutu.querySelectorAll(".category-svg-path");
-    const metoTexts = kutu.querySelectorAll(".slider-category-name");
-
-    if (paths) {
-      paths.forEach((path) => {
+    if (categoriesPaths) {
+      categoriesPaths.forEach((path) => {
         path.style.stroke = "white";
       });
     }
-    if (metoTexts) {
-      metoTexts.forEach((metoText) => {
-        metoText.style.color = "white";
+    if (categoriesTitle) {
+      categoriesTitle.forEach((categoryTitle) => {
+        categoryTitle.style.color = "white";
       });
     }
 
-    kutu.style.backgroundColor = "red";
+    categories.style.backgroundColor = "rgba(219, 68, 68, 1)";
   });
 
-  kutu.addEventListener("mouseleave", (event) => {
-    const paths = kutu.querySelectorAll(".category-svg-path");
-    const metoTexts = kutu.querySelectorAll(".slider-category-name");
+  categories.addEventListener("mouseleave", (event) => {
+    const categoriesPaths = categories.querySelectorAll(".category-svg-path");
+    const categoriesTitle = categories.querySelectorAll(".slider-category-name");
 
-    if (paths) {
-      paths.forEach((path) => {
+    if (categoriesPaths) {
+      categoriesPaths.forEach((path) => {
         path.style.stroke = "black";
       });
     }
-    if (metoTexts) {
-      metoTexts.forEach((metoText) => {
-        metoText.style.color = "black";
+    if (categoriesTitle) {
+      categoriesTitle.forEach((categoryTitle) => {
+        categoryTitle.style.color = "black";
       });
     }
-    kutu.style.backgroundColor = "white";
+    categories.style.backgroundColor = "white";
   });
 });
